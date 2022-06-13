ce89033b76d8aff70945d1eb51d8fe57fbaf8945
sexychoco <gaeun819@naver.com>
2022-06-13 21:48:09 +0900

    selectors

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,19 +1,18 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
-export interface ITodo {
-  id: number;
-  text: string;
-}
+export const minuteState = atom({
+  key: "minutes",
+  default: 0,
+});
 
-interface IToDoState {
-  [key: string]: ITodo[];
-}
-
-export const toDoState = atom<IToDoState>({
-  key: "toDo",
-  default: {
-    To_Do: [],
-    Doing: [],
-    Done: [],
+export const hourSelector = selector<number>({
+  key: "hours",
+  get: ({ get }) => {
+    const minutes = get(minuteState);
+    return minutes / 60;
+  },
+  set: ({ set }, newValue) => {
+    const minutes = Number(newValue) * 60;
+    set(minuteState, minutes);
   },
 });
--- src/styled.d.ts
+++ src/styled.d.ts
@@ -2,8 +2,9 @@ import "styled-components";
 
 declare module "styled-components" {
   export interface DefaultTheme {
+    textColor: string;
     bgColor: string;
-    boardColor: string;
-    cardColor: string;
+    accentColor: string;
+    cardBgColor: string;
   }
 }
